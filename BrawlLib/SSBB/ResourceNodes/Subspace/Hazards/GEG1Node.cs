572b039966297ab16d24d3ccf14a719786f05844
MarioDox <MarioTheDox@gmail.com>
2018-06-30 11:49:54 +0200

    Parsed new enemy names and added new values to GEG1
    Added new Enemy ID value for GEG1 archives
    Added new Enemy Brres ID for GEG1 archives
    Parsed new Enemy names for GEG1 archives

--- BrawlLib/SSBB/ResourceNodes/Subspace/Hazards/GEG1Node.cs
+++ BrawlLib/SSBB/ResourceNodes/Subspace/Hazards/GEG1Node.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using BrawlLib.SSBBTypes;
 using System.ComponentModel;
 
@@ -46,18 +46,93 @@ namespace BrawlLib.SSBB.ResourceNodes
         {
             get
             {
-                int Enemy = *(byte*)(WorkingUncompressed.Address + 0x1D);
+                int Enemy = *(byte*)(WorkingUncompressed.Address + 0x1D); //enemy id is half of their param brres id
                 switch (Enemy)
                 {
+                    case 0: return "Goomba";
+                    case 1: return "Poppant";
+                    case 2: return "Feyesh";
+                    case 3: return "Jyk";
+                    case 4: return "Auroros";
+                    case 5: return "Cymul";
+                    case 6: return "Roturret";
+                    case 7: return "Borboras";
+                    case 8: return "Giant Goomba";
+                    case 9: return "Buckot";
+                    case 10: return "Bucculus";
+                    case 11: return "Greap";
+                    case 12: return "Armight";
+                    case 13: return "Bullet Bill";
+                    case 14: return "Roader";
                     case 15: return "Spaak";
-                    case 23: return "Prim";
-                    case 20: return "BoxerPrim";
-                    case 32: return "BoomPrim";
-                    case 35: return "SwordPrim";
+                    case 16: return "Mite";
+                    case 17: return "Ticken";
+                    case 18: return "Towtow";
+                    case 19: return "Hammer Bro";
+                    case 20: return "Bytan";
+                    case 21: return "Floow";
+                    case 22: return "Puppit";
+                    case 23: return "Primid";
+                    case 24: return "Shellpod";
+                    case 25: return "Koopa";
+                    case 26: return "Shaydas";
+                    case 27: return "Bombed";
+                    case 28: return "Metal Primid";
+                    case 29: return "Nagagog";
+                    case 30: return "Trowlon";
+                    case 31: return "Big Primid";
+                    case 32: return "Boomerang Primid";
+                    case 33: return "Fire Primid";
+                    case 34: return "Scope Primid";
+                    case 35: return "Sword Primid";
+                    case 36: return "Gamyga";
+                    case 37: return "R.O.B. Blaster";
+                    case 38: return "R.O.B. Distance (?)";
+                    case 39: return "R.O.B. Launcher";
+                    case 40: return "R.O.B. Sentry";
+                    case 41: return "Autolance";
+                    case 42: return "Armank";
+                    case 43: return "Glire";
+                    case 44: return "Glice";
+                    case 45: return "Glunder";
+                    case 46: return "Petey Piranha";
+                    case 47: return "Gamyga Base";
+                    case 51: return "Galleom";
+                    case 52: return "Ridley";
+                    case 53: return "Rayquaza";
+                    case 54: return "Duon";
+                    case 55: return "Porky";
+                    case 56: return "Meta Ridley";
+                    case 57: return "Falcon Flyer";
+                    case 58: return "Tabuu";
+                    case 59: return "Master Hand";
+                    case 60: return "Crazy Hand";
                     default: return "Unknown";
                 }
             }
         }
+
+        [Category("Enemy Info")]
+        [DisplayName("Enemy ID")]
+        public int EnemyID
+        {
+            get
+            {
+                return *(byte*)(WorkingUncompressed.Address + 0x1D);
+            }
+        }
+
+        [Category("Enemy Info")]
+        [DisplayName("Enemy Brres ID")]
+        public int EnemyBID
+        {
+            get
+            {
+                return *(byte*)(WorkingUncompressed.Address + 0x1D)*2;
+            }
+        }
+
+
         public override bool OnInitialize()
         {
             base.OnInitialize();
