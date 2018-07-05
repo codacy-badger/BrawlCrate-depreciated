cf330903cd5b8aa0bc9d1640d5b8a05a88114728
soopercool101 <soopercoolstages@gmail.com>
2018-07-05 14:31:33 -0700

    GFG1 initial variables set up

--- BrawlLib/SSBB/ResourceNodes/Subspace/Hazards/GFG1Node.cs
+++ BrawlLib/SSBB/ResourceNodes/Subspace/Hazards/GFG1Node.cs
@@ -37,8 +37,83 @@ namespace BrawlLib.SSBB.ResourceNodes
     public unsafe class GFG1EntryNode : ResourceNode
     {
         internal GFG1Entry* Header { get { return (GFG1Entry*)WorkingUncompressed.Address; } }
-        public override ResourceType ResourceType { get { return ResourceType.ENEMY; } }
-        
+        //public override ResourceType ResourceType { get { return ResourceType.GFG1ENTRY; } }
+
+        public byte _unknown0x01;
+        public byte _unknown0x02;
+        public byte _unknown0x03;
+        public byte _unknown0x04;
+        public byte _unknown0x05;
+        public byte _unknown0x06;
+        public byte _unknown0x07;
+        public byte _unknown0x08;
+        public byte _unknown0x09;
+        public byte _unknown0x0A;
+        public byte _unknown0x0B;
+        public byte _unknown0x0C;
+        public byte _unknown0x0D;
+        public byte _unknown0x0E;
+        public byte _unknown0x0F;
+        public byte _unknown0x10;
+        public byte _unknown0x11;
+        public byte costumeID;      //0x12
+        public byte _flag0x13;
+        public byte _unknown0x14;
+        public byte _unknown0x15;
+        public byte _unknown0x16;
+        public byte _unknown0x17;
+        public byte _unknown0x18;
+        public byte _unknown0x19;
+        public byte _unknown0x1A;
+        public byte _unknown0x1B;
+        public byte _unknown0x1C;
+        public byte _unknown0x1D;
+        public byte _unknown0x1E;
+        public byte _unknown0x1F;
+        public byte _unknown0x20;
+        public byte _unknown0x21;
+        public byte _unknown0x22;
+        public byte _unknown0x23;
+        public bfloat _float0x24;
+        public bfloat _float0x28;
+        public bfloat scale;        // 0x2C
+        public byte _unknown0x30;
+        public byte _unknown0x31;
+        public byte _unknown0x32;
+        public byte _unknown0x33;
+        public byte _unknown0x34;
+        public byte _unknown0x35;
+        public byte _unknown0x36;
+        public byte _unknown0x37;
+        public byte _unknown0x38;
+        public byte _unknown0x39;
+        public byte _unknown0x3A;
+        public byte _unknown0x3B;
+        public byte _unknown0x3C;
+        public byte _unknown0x3D;
+        public byte _unknown0x3E;
+        public byte _unknown0x3F;
+        public byte _unknown0x40;
+        public byte _unknown0x41;
+        public byte _unknown0x42;
+        public byte _unknown0x43;
+        public byte _unknown0x44;
+        public byte _unknown0x45;
+        public byte _unknown0x46;
+        public byte _unknown0x47;
+        public byte _unknown0x48;
+        public byte _unknown0x49;
+        public byte _unknown0x4A;
+        public byte _unknown0x4B;
+        public byte _unknown0x4C;
+        public byte _unknown0x4D;
+        public byte _unknown0x4E;
+        public byte _unknown0x4F;
+        public byte _unknown0x50;
+        public byte _unknown0x51;
+        public byte _unknown0x52;
+        public byte _unknown0x53;
+
         public override bool OnInitialize()
         {
             base.OnInitialize();
