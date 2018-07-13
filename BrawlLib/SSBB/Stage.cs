5d84fa797cdc233b02c4ca7d1380b277475c5aed
soopercool101 <soopercoolstages@gmail.com>
2018-07-13 00:10:22 -0700

    Updates to the stagelist and improved base stage data structure

--- BrawlLib/SSBB/Stage.cs
+++ BrawlLib/SSBB/Stage.cs
@@ -34,10 +34,15 @@ namespace BrawlLib.SSBB
         public int[] UsedCollisionDatas { get; private set; }
         
         /// <summary>
-        /// What ModelData the main StagePosition is stored in
+        /// What ModelData the main StagePosition is stored in (if there are multiple stagepositions, list them elsewhere)
         /// </summary>
-        public int StagePositionModelData { get; private set; }
-        
+        public int MainStagePositionModelData { get; private set; }
+
+        /// <summary>
+        /// What ModelDatas contain StagePositions if there are more than one
+        /// </summary>
+        public int[] AllStagePositionsModelData { get; private set; }
+
         /// <summary>
         /// Index of the Stage Parameters
         /// </summary>
@@ -68,12 +73,13 @@ namespace BrawlLib.SSBB
             this.PacBasename = pac_basename;
         }
         
-        public Stage(byte id, string name, string relname, string pac_basename, int stagePosMD, int stpm, int scla, int stdt, int[] usedCD, int[] usedMD) {
+        public Stage(byte id, string name, string relname, string pac_basename, int stagePosMD, int[] exStagePosMD, int stpm, int scla, int stdt, int[] usedCD, int[] usedMD) {
             this.ID = id;
             this.Name = name;
             this.RelName = relname;
             this.PacBasename = pac_basename;
-            this.StagePositionModelData = stagePosMD;
+            this.MainStagePositionModelData = stagePosMD;
+            this.AllStagePositionsModelData = exStagePosMD;
             this.STPMIndex = stpm;
             this.SCLAIndex = scla;
             this.STDTIndex = stdt;
@@ -114,10 +120,10 @@ namespace BrawlLib.SSBB
         }
 
         public readonly static Stage[] Stages = new Stage[] {
-            //        ID    Display Name                .rel filename           Name without STG        Stage Position      STPM ID     SCLA ID     STDT ID     Collision Datas             Model Datas
+            //        ID    Display Name                .rel filename           Name without STG
             new Stage(0x00, "STGCUSTOM##.pac",          "st_custom##.rel",      "custom"),
-            new Stage(0x01, "Battlefield",              "st_battle.rel",        "battlefield",          100,                10,         30,         -1,         new int[] {2},              new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 100, 101, 200}),
-            new Stage(0x02, "Final Destination",        "st_final.rel",         "final",                100,                10,         30,         -1,         new int[] {2},              new int[] {1, 2, 100, 101}),
+            new Stage(0x01, "Battlefield",              "st_battle.rel",        "battlefield"),
+            new Stage(0x02, "Final Destination",        "st_final.rel",         "final"),
             new Stage(0x03, "Delfino Plaza",            "st_dolpic.rel",        "dolpic"),
             new Stage(0x04, "Luigi's Mansion",          "st_mansion.rel",       "mansion"),
             new Stage(0x05, "Mushroomy Kingdom",        "st_mariopast.rel",     "mariopast"),
@@ -130,7 +136,7 @@ namespace BrawlLib.SSBB
             new Stage(0x0D, "Yoshi's Island (Brawl)",   "st_crayon.rel",        "crayon"),
             new Stage(0x0E, "Halberd",                  "st_halberd.rel",       "halberd"),
             new Stage(0x13, "Lylat Cruise",             "st_starfox.rel",       "starfox"),
-            new Stage(0x14, "Pokemon Stadium 2",        "st_stadium.rel",       "stadium"),
+            new Stage(0x14, "Pokémon Stadium 2",        "st_stadium.rel",       "stadium"),
             new Stage(0x15, "Spear Pillar",             "st_tengan.rel",        "tengan"),
             new Stage(0x16, "Port Town Aero Dive",      "st_fzero.rel",         "fzero"),
             new Stage(0x17, "Summit",                   "st_ice.rel",           "ice"),
@@ -152,17 +158,17 @@ namespace BrawlLib.SSBB
             new Stage(0x2B, "Jungle Japes",             "st_dxgarden.rel",      "dxgarden"),
             new Stage(0x2C, "Onett",                    "st_dxonett.rel",       "dxonett"),
             new Stage(0x2D, "Green Greens",             "st_dxgreens.rel",      "dxgreens"),
-            new Stage(0x2E, "Pokemon Stadium",          "st_dxpstadium.rel",    "dxpstadium"),
+            new Stage(0x2E, "Pokémon Stadium",          "st_dxpstadium.rel",    "dxpstadium"),
             new Stage(0x2F, "Rainbow Cruise",           "st_dxrcruise.rel",     "dxrcruise"),
             new Stage(0x30, "Corneria",                 "st_dxcorneria.rel",    "dxcorneria"),
             new Stage(0x31, "Big Blue",                 "st_dxbigblue.rel",     "dxbigblue"),
             new Stage(0x32, "Brinstar",                 "st_dxzebes.rel",       "dxzebes"),
             new Stage(0x33, "Bridge of Eldin",          "st_oldin.rel",         "oldin"),
             new Stage(0x34, "Homerun",                  "st_homerun.rel",       "homerun"),
-            new Stage(0x35, "Edit",                     "st_stageedit.rel",     "edit"),
-            new Stage(0x36, "Heal",                     "st_heal.rel",          "heal"),
+            new Stage(0x35, "Stage Builder",            "st_stageedit.rel",     "edit"),
+            new Stage(0x36, "All-Star Rest Area",       "st_heal.rel",          "heal"),
             new Stage(0x37, "Online Training",          "st_otrain.rel",        "onlinetraining"),
-            new Stage(0x38, "TargetBreak",              "st_tbreak.rel",        "targetlv"),
+            new Stage(0x38, "Target Break",             "st_tbreak.rel",        "targetlv"),
             new Stage(0x39, "Classic mode credits",     "st_croll.rel",         "chararoll"),
             new Stage(0x40, "Custom 01",                "st_custom01.rel",      "custom01"),
             new Stage(0x41, "Custom 02",                "st_custom02.rel",      "custom02"),
@@ -173,7 +179,7 @@ namespace BrawlLib.SSBB
             new Stage(0x46, "Custom 07",                "st_custom07.rel",      "custom07"),
             new Stage(0x47, "Custom 08",                "st_custom08.rel",      "custom08"),
             new Stage(0x48, "Custom 09",                "st_custom09.rel",      "custom09"),
-            new Stage(0x49, "Custom0 A",                "st_custom0A.rel",      "custom0A"),
+            new Stage(0x49, "Custom 0A",                "st_custom0A.rel",      "custom0A"),
             new Stage(0x4A, "Custom 0B",                "st_custom0B.rel",      "custom0B"),
             new Stage(0x4B, "Custom 0C",                "st_custom0C.rel",      "custom0C"),
             new Stage(0x4C, "Custom 0D",                "st_custom0D.rel",      "custom0D"),
@@ -356,5 +362,58 @@ namespace BrawlLib.SSBB
             new Stage(0xFD, "Custom BE",                "st_customBE.rel",      "customBE"),
             new Stage(0xFE, "Custom BF",                "st_customBF.rel",      "customBF"),
         };
+
+        public readonly static Stage[] BaseStages = new Stage[] {
+            //        ID    Display Name                    .rel filename           Name without STG        Stage Position (Main, then all)                                                     STPM ID     SCLA ID     STDT ID     Collision Datas                                                     Model Datas
+            new Stage(0x01, "Battlefield",                  "st_battle.rel",        "battlefield",          100, new int[] {100 },                                                              10,         30,         -1,         new int[] {2 },                                                     new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 100, 101, 200 }),
+            new Stage(0x02, "Final Destination",            "st_final.rel",         "final",                100, new int[] {100 },                                                              10,         30,         -1,         new int[] {2 },                                                     new int[] {1, 2, 100, 101 }),
+            new Stage(0x03, "Delfino Plaza",                "st_dolpic.rel",        "dolpic",               100, new int[] {100, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123 },  10,         30,         20,         new int[] {3, 4, 5, 6, 7, 50, 51, 52, 53, 54, 55, 56, 57, 58 },     new int[] {0, 1, 4, 5, 6, 7, 8, 9, 10, 12, 13, 22, 23, 32, 33, 42, 43, 44, 50, 100, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123 }),
+            new Stage(0x04, "Luigi's Mansion",              "st_mansion.rel",       "mansion",              100, new int[] {100 },                                                              10,         30,         20,         new int[] {2 },                                                     new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14 }),
+            new Stage(0x05, "Mushroomy Kingdom (1-1)",      "st_mariopast.rel",     "mariopast_00",         100, new int[] {100 },                                                              10,         30,         20,         new int[] {2 },                                                     new int[] {0, 1, 2, 90, 100, 101, 102, 103, 104 }),
+            new Stage(0x05, "Mushroomy Kingdom (1-2)",      "st_mariopast.rel",     "mariopast_01",         100, new int[] {100 },                                                              10,         30,         20,         new int[] {2, 5 },                                                  new int[] {0, 1, 2, 4, 90, 100, 101, 102, 103, 104 }),
+            new Stage(0x06, "Mario Circuit",                "st_kart.rel",          "kart"),
+            new Stage(0x07, "75 m",                         "st_donkey.rel",        "donkey"),
+            new Stage(0x08, "Rumble Falls",                 "st_jungle.rel",        "jungle"),
+            new Stage(0x09, "Pirate Ship",                  "st_pirates.rel",       "pirates"),
+            new Stage(0x0B, "Norfair",                      "st_norfair.rel",       "norfair"),
+            new Stage(0x0C, "Frigate Orpheon",              "st_orpheon.rel",       "orpheon"),
+            new Stage(0x0D, "Yoshi's Island (Brawl)",       "st_crayon.rel",        "crayon"),
+            new Stage(0x0E, "Halberd",                      "st_halberd.rel",       "halberd"),
+            new Stage(0x13, "Lylat Cruise",                 "st_starfox.rel",       "starfox"),
+            new Stage(0x14, "Pokémon Stadium 2",            "st_stadium.rel",       "stadium"),
+            new Stage(0x15, "Spear Pillar",                 "st_tengan.rel",        "tengan"),
+            new Stage(0x16, "Port Town Aero Dive",          "st_fzero.rel",         "fzero"),
+            new Stage(0x17, "Summit",                       "st_ice.rel",           "ice"),
+            new Stage(0x18, "Flat Zone 2",                  "st_gw.rel",            "gw"),
+            new Stage(0x19, "Castle Siege",                 "st_emblem.rel",        "emblem"),
+            new Stage(0x1C, "WarioWare Inc.",               "st_madein.rel",        "madein"),
+            new Stage(0x1D, "Distant Planet",               "st_earth.rel",         "earth"),
+            new Stage(0x1E, "Skyworld",                     "st_palutena.rel",      "palutena"),
+            new Stage(0x1F, "Mario Bros.",                  "st_famicom.rel",       "famicom"),
+            new Stage(0x20, "New Pork City",                "st_newpork.rel",       "newpork"),
+            new Stage(0x21, "Smashville",                   "st_village.rel",       "village"),
+            new Stage(0x22, "Shadow Moses Island",          "st_metalgear.rel",     "metalgear"),
+            new Stage(0x23, "Green Hill Zone",              "st_greenhill.rel",     "greenhill"),
+            new Stage(0x24, "PictoChat",                    "st_pictchat.rel",      "pictchat"),
+            new Stage(0x25, "Hanenbow",                     "st_plankton.rel",      "plankton"),
+            new Stage(0x26, "Control Configuration Test",   "st_config.rel",        "configtest"),
+            new Stage(0x29, "Temple",                       "st_dxshrine.rel",      "dxshrine"),
+            new Stage(0x2A, "Yoshi's Island (Melee)",       "st_dxyorster.rel",     "dxyorster"),
+            new Stage(0x2B, "Jungle Japes",                 "st_dxgarden.rel",      "dxgarden"),
+            new Stage(0x2C, "Onett",                        "st_dxonett.rel",       "dxonett"),
+            new Stage(0x2D, "Green Greens",                 "st_dxgreens.rel",      "dxgreens"),
+            new Stage(0x2E, "Pokémon Stadium",              "st_dxpstadium.rel",    "dxpstadium"),
+            new Stage(0x2F, "Rainbow Cruise",               "st_dxrcruise.rel",     "dxrcruise"),
+            new Stage(0x30, "Corneria",                     "st_dxcorneria.rel",    "dxcorneria"),
+            new Stage(0x31, "Big Blue",                     "st_dxbigblue.rel",     "dxbigblue"),
+            new Stage(0x32, "Brinstar",                     "st_dxzebes.rel",       "dxzebes"),
+            new Stage(0x33, "Bridge of Eldin",              "st_oldin.rel",         "oldin"),
+            new Stage(0x34, "Homerun",                      "st_homerun.rel",       "homerun"),
+            new Stage(0x35, "Stage Builder",                "st_stageedit.rel",     "edit"),
+            new Stage(0x36, "All Star Rest Area",           "st_heal.rel",          "heal"),
+            new Stage(0x37, "Online Training",              "st_otrain.rel",        "onlinetraining"),
+            new Stage(0x38, "Target Break",                 "st_tbreak.rel",        "targetlv"),
+            new Stage(0x39, "Classic mode credits",         "st_croll.rel",         "chararoll"),
+        }
     }
 }
