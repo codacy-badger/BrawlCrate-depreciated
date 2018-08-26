6e18acd2b70872d79ea5c15678ce4eb006e98c3a
soopercool101 <soopercoolstages@gmail.com>
2018-08-26 15:07:58 -0700

    Improved tmpfolder functionality.
    - Tmp directory gets deleted entirely on starting the program
    - Tmp directory is now hidden
    - Tmp directory no longer requires subfolders
    - Now randomly generates a 6 digit hex value for each tmp file, fixing bug where trying to do a new 1:1 of the same base you had open would crash

--- BrawlBox/Program.cs
+++ BrawlBox/Program.cs
@@ -90,6 +90,7 @@ namespace BrawlBox
         [STAThread]
         public static void Main(string[] args)
         {
+            Generate1to1Stages.clearTmpDir(Generate1to1Stages.tmpDirectory);
             if (args.Length >= 1)
             {
                 if (args[0].Equals("/gct", StringComparison.InvariantCultureIgnoreCase))
--- BrawlLib/BrawlCrate/Generate1to1Stages.cs
+++ BrawlLib/BrawlCrate/Generate1to1Stages.cs
@@ -9,23 +9,48 @@ namespace BrawlLib.BrawlCrate
 {
     public class Generate1to1Stages
     {
-        public static string tmpDirectory = AppDomain.CurrentDomain.BaseDirectory + '\\' + "tmp";
-        public static string stageDirectory = tmpDirectory + '\\' + "Stages";
+        public static string tmpDirectory = TmpDirGen();
 
-        public static string pm36stgfinalDirectory = stageDirectory + '\\' + "STGPM36FINAL.pac";
+        public static string TmpDirGen()
+        {
+            string tmp = AppDomain.CurrentDomain.BaseDirectory;
+            if (!tmp.EndsWith("\\"))
+                tmp += '\\';
+            tmp += "tmp";
+            return tmp;
+        }
+        
+        public static uint randnumgen = 0;
+
+        public static string pm36stgfinalDirectory = tmpDirectory + '\\' + randnumgen.ToString("X8").Substring(2, 6) + "STGPM36FINAL.pac";
         public static bool PM36STGFINAL()
         {
-            Directory.CreateDirectory(tmpDirectory);
-            Directory.CreateDirectory(stageDirectory);
+            Random rand = new Random();
+            byte[] buf = new byte[4];
+            rand.NextBytes(buf);
+            randnumgen = BitConverter.ToUInt32(buf, 0);
+            pm36stgfinalDirectory = tmpDirectory + '\\' + randnumgen.ToString("X8").Substring(2, 6) + "STGPM36FINAL.pac";
+            DirectoryInfo tmp = Directory.CreateDirectory(tmpDirectory);
+            tmp.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
             // Use TLS 1.2, used by GitHub
             ServicePointManager.SecurityProtocol |= (SecurityProtocolType)3072;
-
             using (WebClient client = new WebClient())
             {
                 client.DownloadFile("https://github.com/soopercool101/Stage-Templates-1to1s/raw/master/Project%20M/3.6/STGFINAL.pac",
                                     @pm36stgfinalDirectory);
             }
             return File.Exists(pm36stgfinalDirectory);
+        }
+
+        public static void clearTmpDir(string directory)
+        {
+            Directory.CreateDirectory(directory);
+            DirectoryInfo tmpdir = new DirectoryInfo(directory);
+            foreach (DirectoryInfo dir in tmpdir.GetDirectories())
+                clearTmpDir(dir.FullName);
+            foreach (FileInfo file in tmpdir.GetFiles())
+                file.Delete();
+            tmpdir.Delete();
         }
     }
 }
