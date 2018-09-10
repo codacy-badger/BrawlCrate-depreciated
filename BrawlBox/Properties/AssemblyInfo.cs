9a601ebfdb7c940266cdbe8bf0fa3bd3559f804a
soopercool101 <soopercoolstages@gmail.com>
2018-09-09 17:45:39 -0700

    Prepare for Hotfix release

--- BrawlBox/Properties/AssemblyInfo.cs
+++ BrawlBox/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ Super Smash Bros. Brawl © 2008 Nintendo and HAL Laboratory")]
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.10.*")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.1")]
