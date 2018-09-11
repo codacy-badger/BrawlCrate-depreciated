f1807652300baef48a5908274bc4b5f4f97d27af
SammiHusky <sammi-husky@LIVE.COM>
2018-09-11 15:39:24 -0700

    Cleanup BLOC files and document the 2 unknown Fields (Version and ExtParam)

--- BrawlLib/SSBB/Types/Subspace/BLOC.cs
+++ BrawlLib/SSBB/Types/Subspace/BLOC.cs
@@ -12,38 +12,11 @@ namespace BrawlLib.SSBBTypes
 
         public uint _tag;
         public bint _count;
-        public bint _unk0;
-        public int _pad1;
+        public bint _version;
+        public int _extParam;
 
         public VoidPtr this[int index] { get { return (VoidPtr)((byte*)Address + Offsets(index)); } }
         public uint Offsets(int index) { return *(buint*)((byte*)Address + 0x10 + (index * 4)); }
-        private VoidPtr Address { get { fixed (void* ptr = &this)return ptr; } }
-
-        public BLOC(int count)
-        {
-            _tag = Tag;
-            _count = count;
-            _unk0 = 0x80;
-            _pad1 = 0x00;
-        }
-    }
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public unsafe struct BLOCEntry
-    {
-        //public bushort _id;
-        //public byte _id2;
-        //public byte _echo;
-
-        //public fixed int _values[64];
-
-        //public BLOCEntry(ushort id, byte echo, byte id2)
-        //{
-        //    _id = id;
-        //    _echo = echo;
-        //    _id2 = id2;
-        //}
-
         private VoidPtr Address { get { fixed (void* ptr = &this)return ptr; } }
     }
 }
