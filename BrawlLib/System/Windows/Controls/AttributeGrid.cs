d20775449cc47fb015ac828a9091e8b7f9db0a58
soopercool101 <soopercoolstages@gmail.com>
2018-10-08 13:11:46 -0700

    Fix bug in which viewing an STDT with 0 entries would crash the program (STGHOMERUN.pac in vBrawl)

--- BrawlLib/System/Windows/Controls/AttributeGrid.cs
+++ BrawlLib/System/Windows/Controls/AttributeGrid.cs
@@ -22,11 +22,11 @@ namespace System.Windows.Forms
             this.lblColor = new System.Windows.Forms.Label();
             this.lblCNoA = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.rdoUnknown = new System.Windows.Forms.RadioButton();
-            this.rdoDegrees = new System.Windows.Forms.RadioButton();
-            this.rdoColor = new System.Windows.Forms.RadioButton();
-            this.rdoInt = new System.Windows.Forms.RadioButton();
             this.rdoFloat = new System.Windows.Forms.RadioButton();
+            this.rdoInt = new System.Windows.Forms.RadioButton();
+            this.rdoColor = new System.Windows.Forms.RadioButton();
+            this.rdoDegrees = new System.Windows.Forms.RadioButton();
+            this.rdoUnknown = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.dtgrdAttributes)).BeginInit();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
@@ -113,7 +113,7 @@ namespace System.Windows.Forms
             // 
             this.btnInf.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnInf.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInf.Location = new System.Drawing.Point(440, 44);
+            this.btnInf.Location = new System.Drawing.Point(446, 44);
             this.btnInf.Name = "btnInf";
             this.btnInf.Size = new System.Drawing.Size(30, 30);
             this.btnInf.TabIndex = 13;
@@ -126,7 +126,7 @@ namespace System.Windows.Forms
             // 
             this.btnMinusInf.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMinusInf.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMinusInf.Location = new System.Drawing.Point(406, 44);
+            this.btnMinusInf.Location = new System.Drawing.Point(412, 44);
             this.btnMinusInf.Name = "btnMinusInf";
             this.btnMinusInf.Size = new System.Drawing.Size(30, 30);
             this.btnMinusInf.TabIndex = 12;
@@ -179,53 +179,21 @@ namespace System.Windows.Forms
             this.tableLayoutPanel1.Size = new System.Drawing.Size(479, 25);
             this.tableLayoutPanel1.TabIndex = 9;
             // 
-            // rdoUnknown
+            // rdoFloat
             // 
-            this.rdoUnknown.Appearance = System.Windows.Forms.Appearance.Button;
-            this.rdoUnknown.AutoSize = true;
-            this.rdoUnknown.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rdoUnknown.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdoUnknown.Location = new System.Drawing.Point(380, 0);
-            this.rdoUnknown.Margin = new System.Windows.Forms.Padding(0);
-            this.rdoUnknown.Name = "rdoUnknown";
-            this.rdoUnknown.Size = new System.Drawing.Size(99, 25);
-            this.rdoUnknown.TabIndex = 1;
-            this.rdoUnknown.TabStop = true;
-            this.rdoUnknown.Text = "Hexidecimal";
-            this.rdoUnknown.UseVisualStyleBackColor = true;
-            this.rdoUnknown.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
-            // 
-            // rdoDegrees
-            // 
-            this.rdoDegrees.Appearance = System.Windows.Forms.Appearance.Button;
-            this.rdoDegrees.AutoSize = true;
-            this.rdoDegrees.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rdoDegrees.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdoDegrees.Location = new System.Drawing.Point(285, 0);
-            this.rdoDegrees.Margin = new System.Windows.Forms.Padding(0);
-            this.rdoDegrees.Name = "rdoDegrees";
-            this.rdoDegrees.Size = new System.Drawing.Size(95, 25);
-            this.rdoDegrees.TabIndex = 2;
-            this.rdoDegrees.TabStop = true;
-            this.rdoDegrees.Text = "Degrees";
-            this.rdoDegrees.UseVisualStyleBackColor = true;
-            this.rdoDegrees.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
-            // 
-            // rdoColor
-            // 
-            this.rdoColor.Appearance = System.Windows.Forms.Appearance.Button;
-            this.rdoColor.AutoSize = true;
-            this.rdoColor.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rdoColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdoColor.Location = new System.Drawing.Point(190, 0);
-            this.rdoColor.Margin = new System.Windows.Forms.Padding(0);
-            this.rdoColor.Name = "rdoColor";
-            this.rdoColor.Size = new System.Drawing.Size(95, 25);
-            this.rdoColor.TabIndex = 1;
-            this.rdoColor.TabStop = true;
-            this.rdoColor.Text = "Color";
-            this.rdoColor.UseVisualStyleBackColor = true;
-            this.rdoColor.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
+            this.rdoFloat.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rdoFloat.AutoSize = true;
+            this.rdoFloat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdoFloat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoFloat.Location = new System.Drawing.Point(0, 0);
+            this.rdoFloat.Margin = new System.Windows.Forms.Padding(0);
+            this.rdoFloat.Name = "rdoFloat";
+            this.rdoFloat.Size = new System.Drawing.Size(95, 25);
+            this.rdoFloat.TabIndex = 0;
+            this.rdoFloat.TabStop = true;
+            this.rdoFloat.Text = "Float";
+            this.rdoFloat.UseVisualStyleBackColor = true;
+            this.rdoFloat.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
             // 
             // rdoInt
             // 
@@ -243,21 +211,53 @@ namespace System.Windows.Forms
             this.rdoInt.UseVisualStyleBackColor = true;
             this.rdoInt.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
             // 
-            // rdoFloat
+            // rdoColor
             // 
-            this.rdoFloat.Appearance = System.Windows.Forms.Appearance.Button;
-            this.rdoFloat.AutoSize = true;
-            this.rdoFloat.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rdoFloat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdoFloat.Location = new System.Drawing.Point(0, 0);
-            this.rdoFloat.Margin = new System.Windows.Forms.Padding(0);
-            this.rdoFloat.Name = "rdoFloat";
-            this.rdoFloat.Size = new System.Drawing.Size(95, 25);
-            this.rdoFloat.TabIndex = 0;
-            this.rdoFloat.TabStop = true;
-            this.rdoFloat.Text = "Float";
-            this.rdoFloat.UseVisualStyleBackColor = true;
-            this.rdoFloat.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
+            this.rdoColor.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rdoColor.AutoSize = true;
+            this.rdoColor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdoColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoColor.Location = new System.Drawing.Point(190, 0);
+            this.rdoColor.Margin = new System.Windows.Forms.Padding(0);
+            this.rdoColor.Name = "rdoColor";
+            this.rdoColor.Size = new System.Drawing.Size(95, 25);
+            this.rdoColor.TabIndex = 1;
+            this.rdoColor.TabStop = true;
+            this.rdoColor.Text = "Color";
+            this.rdoColor.UseVisualStyleBackColor = true;
+            this.rdoColor.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
+            // 
+            // rdoDegrees
+            // 
+            this.rdoDegrees.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rdoDegrees.AutoSize = true;
+            this.rdoDegrees.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdoDegrees.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoDegrees.Location = new System.Drawing.Point(285, 0);
+            this.rdoDegrees.Margin = new System.Windows.Forms.Padding(0);
+            this.rdoDegrees.Name = "rdoDegrees";
+            this.rdoDegrees.Size = new System.Drawing.Size(95, 25);
+            this.rdoDegrees.TabIndex = 2;
+            this.rdoDegrees.TabStop = true;
+            this.rdoDegrees.Text = "Degrees";
+            this.rdoDegrees.UseVisualStyleBackColor = true;
+            this.rdoDegrees.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
+            // 
+            // rdoUnknown
+            // 
+            this.rdoUnknown.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rdoUnknown.AutoSize = true;
+            this.rdoUnknown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdoUnknown.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoUnknown.Location = new System.Drawing.Point(380, 0);
+            this.rdoUnknown.Margin = new System.Windows.Forms.Padding(0);
+            this.rdoUnknown.Name = "rdoUnknown";
+            this.rdoUnknown.Size = new System.Drawing.Size(99, 25);
+            this.rdoUnknown.TabIndex = 1;
+            this.rdoUnknown.TabStop = true;
+            this.rdoUnknown.Text = "Hexidecimal";
+            this.rdoUnknown.UseVisualStyleBackColor = true;
+            this.rdoUnknown.CheckedChanged += new System.EventHandler(this.radioButtonsChanged);
             // 
             // AttributeGrid
             // 
@@ -488,7 +488,14 @@ namespace System.Windows.Forms
             lblColor.Visible = false;
             lblCNoA.Visible = false;
             btnInf.Visible = btnMinusInf.Visible = false;
+            rdoColor.Enabled = rdoDegrees.Enabled = rdoFloat.Enabled = rdoInt.Enabled = rdoUnknown.Enabled = true;
             if (dtgrdAttributes.CurrentCell == null) return;
+            if(AttributeArray.Length <= 0)
+            {
+                description.Text = "";
+                rdoColor.Enabled = rdoDegrees.Enabled = rdoFloat.Enabled = rdoInt.Enabled = rdoUnknown.Enabled = false;
+                return;
+            }
             int index = dtgrdAttributes.CurrentCell.RowIndex;
 
             //Display description of the selected attribute.
@@ -519,7 +526,7 @@ namespace System.Windows.Forms
                 return;
 
             int index = dtgrdAttributes.CurrentCell.RowIndex;
-            if (index >= 0)
+            if (index >= 0 && AttributeArray.Length > index)
             {
                 AttributeArray[index]._description = description.Text;
                 if (DictionaryChanged != null) DictionaryChanged.Invoke(this, EventArgs.Empty);
