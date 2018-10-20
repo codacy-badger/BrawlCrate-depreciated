eb24e1e1db5a05ed833b0b637f75f4f803f678eb
soopercool101 <soopercoolstages@gmail.com>
2018-10-20 15:58:55 -0700

    Ensure BrawlLib settings get upgraded on a new release

--- BrawlCrate/UI/MainForm.cs
+++ BrawlCrate/UI/MainForm.cs
@@ -57,9 +57,23 @@ namespace BrawlCrate
 
             if (BrawlCrate.Properties.Settings.Default.UpdateSettings)
             {
-                BrawlCrate.Properties.Settings.Default.Upgrade();
+                foreach (var _Assembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    foreach (var _Type in _Assembly.GetTypes())
+                    {
+                        if (_Type.Name == "Settings" && typeof(SettingsBase).IsAssignableFrom(_Type))
+                        {
+                            var settings = (ApplicationSettingsBase)_Type.GetProperty("Default").GetValue(null, null);
+                            if (settings != null)
+                            {
+                                settings.Upgrade();
+                                settings.Reload();
+                                settings.Save();
+                            }
+                        }
+                    }
+                }
                 BrawlCrate.Properties.Settings.Default.UpdateSettings = false;
-                BrawlCrate.Properties.Settings.Default.Save();
             }
 
             _displayPropertyDescription = BrawlCrate.Properties.Settings.Default.DisplayPropertyDescriptionWhenAvailable;
