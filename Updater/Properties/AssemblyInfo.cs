338485f2a4cadd99602b8c1d2ef26122f87ead00
soopercool101 <soopercoolstages@gmail.com>
2018-10-26 14:23:03 -0700

    Updated updater info

--- Updater/Properties/AssemblyInfo.cs
+++ Updater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brawltools")]
 [assembly: AssemblyProduct("BrawlCrate")]
-[assembly: AssemblyCopyright("Copyright © Tommy Burrows - (Sammi Husky)")]
+[assembly: AssemblyCopyright("Copyright © soopercool101, Tommy Burrows - (Sammi Husky)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
