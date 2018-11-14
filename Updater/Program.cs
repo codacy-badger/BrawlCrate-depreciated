316fcb831526b0d10868a5dc0f77169f55f5e079
soopercool101 <soopercoolstages@gmail.com>
2018-11-14 12:18:25 -0800

    Moved BrawlCrate issues to https://github.com/BrawlCrate/BrawlCrateIssues/issues

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -255,9 +255,9 @@ namespace Net
     {
         static byte[] _rawData = 
         {
-            0x33, 0x62, 0x33, 0x33, 0x36, 0x34, 0x64, 0x32, 0x65, 0x62, 0x65, 0x34, 0x39, 0x36, 0x63, 0x61, 0x62, 0x63,
-            0x32, 0x61, 0x65, 0x37, 0x39, 0x36, 0x37, 0x30, 0x39, 0x63, 0x35, 0x61, 0x61, 0x31, 0x61, 0x61, 0x63, 0x35,
-            0x33, 0x63, 0x34, 0x66
+            0x34, 0x35, 0x31, 0x30, 0x34, 0x31, 0x62, 0x38, 0x65, 0x39, 0x32, 0x64, 0x37, 0x32, 0x66, 0x62, 0x63, 0x36,
+            0x38, 0x62, 0x63, 0x66, 0x61, 0x39, 0x36, 0x61, 0x32, 0x65, 0x30, 0x36, 0x64, 0x62, 0x61, 0x33, 0x62, 0x36,
+            0x39, 0x32, 0x66, 0x63, 0x20
         };
 
         public static async Task CreateIssue(
@@ -269,9 +269,6 @@ namespace Net
         {
             try
             {
-                //Gain access to the StageBox account on github for submitting the report.
-                //I don't really care if this gets compromised, the token has no user settings access so I'll just revoke access to the token and generate a new one.
-                //Have to use a byte array to (hopefully) bypass github's automatic detection of the token as a string.
                 Octokit.Credentials s = new Credentials(System.Text.Encoding.Default.GetString(_rawData));
                 var github = new GitHubClient(new Octokit.ProductHeaderValue("BrawlCrate")) { Credentials = s };
                 IReadOnlyList<Release> releases = null;
@@ -290,7 +287,7 @@ namespace Net
                         releases = releases.Where(r => !r.Prerelease).ToList();
                     }
 
-                    issues = await github.Issue.GetForRepository("StageBoxBrawl", "StageBoxIssues");
+                    issues = await github.Issue.GetForRepository("BrawlCrate", "BrawlCrateIssues");
                 }
                 catch (System.Net.Http.HttpRequestException)
                 {
@@ -337,7 +334,7 @@ namespace Net
                                         Environment.NewLine +
                                         i.Body;
 
-                                    Issue x = await github.Issue.Update("StageBoxBrawl", "StageBoxIssues", i.Number, update);
+                                    Issue x = await github.Issue.Update("BrawlCrate", "BrawlCrateIssues", i.Number, update);
                                 }
                             }
                     
@@ -355,7 +352,7 @@ namespace Net
                             Environment.NewLine +
                             StackTrace
                         };
-                        Issue x = await github.Issue.Create("StageBoxBrawl", "StageBoxIssues", issue);
+                        Issue x = await github.Issue.Create("BrawlCrate", "BrawlCrateIssues", issue);
                     }
                 }
             }
