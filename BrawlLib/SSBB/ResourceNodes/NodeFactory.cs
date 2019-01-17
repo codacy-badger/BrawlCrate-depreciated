7f46b969d49f66e2ab35ca6aa292ada72e41ab20
soopercool101 <soopercoolstages@gmail.com>
2019-01-16 19:47:11 -0800

    Can open and attempt to uncompress stage builder files (NOTE: DOES NOT PROPERLY WORK)

--- BrawlLib/SSBB/ResourceNodes/NodeFactory.cs
+++ BrawlLib/SSBB/ResourceNodes/NodeFactory.cs
@@ -12,11 +12,11 @@ namespace BrawlLib.SSBB.ResourceNodes
     //Factory is for initializing root node, and unknown child nodes.
     public static class NodeFactory
     {
-//#if DEBUG
-//        private const bool UseRawDataNode = true;
-//#else
+#if DEBUG
+        private const bool UseRawDataNode = true;
+#else
         private const bool UseRawDataNode = false;
-//#endif
+#endif
         
         private static List<ResourceParser> _parsers = new List<ResourceParser>();
         static NodeFactory()
@@ -34,6 +34,7 @@ namespace BrawlLib.SSBB.ResourceNodes
             { "MRGC", typeof(MRGNode) }, //Compressed MRG
             { "DOL", typeof(DOLNode) },
             { "REL", typeof(RELNode) },
+            { "BIN", typeof(RawDataNode) },
         };
 
         //Parser commands must initialize the node before returning.
