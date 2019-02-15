f60844b780499ea1861c0aba46bebb4c43031cce
soopercool101 <soopercoolstages@gmail.com>
2019-02-14 16:57:34 -0800

    Improve detection of menu files

--- BrawlCrate/Discord/DiscordSettings.cs
+++ BrawlCrate/Discord/DiscordSettings.cs
@@ -79,7 +79,8 @@ namespace BrawlCrate.Discord
                     }
                 }
                 else if(MainForm.Instance.RootNode.ResourceNode.Name.StartsWith("sc_", StringComparison.OrdinalIgnoreCase) ||
-                        MainForm.Instance.RootNode.ResourceNode.Name.StartsWith("common", StringComparison.OrdinalIgnoreCase))
+                        MainForm.Instance.RootNode.ResourceNode.Name.Equals("common5", StringComparison.OrdinalIgnoreCase) ||
+                        MainForm.Instance.RootNode.ResourceNode.Name.StartsWith("mu_", StringComparison.OrdinalIgnoreCase))
                 {
                     DiscordController.presence.details = workString + " " + "menus";
                 }
