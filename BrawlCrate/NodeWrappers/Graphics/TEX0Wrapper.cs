c74b33a0218dd298cdd86475b52974f2f51f6b2f
soopercool101 <soopercoolstages@gmail.com>
2019-03-06 17:23:39 -0800

    Add fix to continue deletion when color smash temp output directory cannot be deleted

--- BrawlCrate/NodeWrappers/Graphics/TEX0Wrapper.cs
+++ BrawlCrate/NodeWrappers/Graphics/TEX0Wrapper.cs
@@ -209,7 +209,7 @@ namespace BrawlCrate.NodeWrappers
             {
                 foreach (FileInfo tex in outputDir.GetFiles())
                     try { tex.Delete(); } catch { }
-                outputDir.Delete();
+                try { outputDir.Delete(); } catch { }
                 foreach (FileInfo tex in Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\cs\\").GetFiles())
                     try { tex.Delete(); } catch { }
                 Directory.Delete(AppDomain.CurrentDomain.BaseDirectory + "\\cs\\");
