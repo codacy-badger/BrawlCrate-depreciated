f1168eac69169cbb1954ab2a5da0d077dca917a3
soopercool101 <soopercoolstages@gmail.com>
2019-04-17 09:58:52 -0700

    Fix changelog

--- BrawlCrate/Program.cs
+++ BrawlCrate/Program.cs
@@ -18,7 +18,7 @@ namespace BrawlCrate
         //Make sure this matches the tag name of the release on github exactly
         public static readonly string TagName = "BrawlCrate_v0.25b";
         public static readonly string UpdateMessage = @"Updated to BrawlCrate v0.25b! This release:
-- Integrates Color Smash 3.0 functionality, allowing for images with
+- Integrates Color Smash 3.0 functionality, allowing for images with less than 256 colors to be properly color smashed
 
 In case you missed it, BrawlCrate v0.25:
 - Added ability to preview metal materials (press the 'M' key in the model previewer)
